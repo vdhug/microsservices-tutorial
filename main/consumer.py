d4768da0dde4869f8aceabe55a8954b49a71263c
Vitor Hugo da Silva Lima <vitor.lima@cornershopapp.com>
2021-08-21 14:35:47 -0500

    Adding auto_ack to true on consumer basic consume calls

--- main/consumer.py
+++ main/consumer.py
@@ -15,7 +15,7 @@ def callback(channel, method, properties, body):
     print("Received in main")
     print(body)
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print("Started consuming")
 
