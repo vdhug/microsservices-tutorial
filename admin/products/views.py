47c71dd300c635672ae412dcb14a948746e6ac32
Vitor Hugo da Silva Lima <vitor.lima@cornershopapp.com>
2021-08-20 21:40:19 -0500

    Adding list, create, retrieve, update and destroy routes to API

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
+import random
 
-from .models import Product
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Product, User
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ViewSet):
@@ -9,22 +12,48 @@ class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
         # GET /api/products
         products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        serializer = ProductSerializer(instance=products, many=True)
+        reponse_data = Response(serializer.data)
+        return reponse_data
 
 
     def create(self, request):
         # POST /api/products
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response_data = Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return response_data
 
     def retrieve(self, request, pk=None):
         # GET /api/products/<str:id>
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product)
+        response_data = Response(data=serializer.data)
+        return response_data
 
     def update(self, request, pk=None):
         # PUT /api/products/<str:id>
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response_data = Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        return response_data
 
     def destroy(self, request, pk=None):
         # DELETE /api/products/<str:id>
-        pass
+        product = Product.objects.get(id=pk)
+        product.delete()
+        response_data = Response(status=status.HTTP_204_NO_CONTENT)
+        return response_data
+
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id
+        })
+
