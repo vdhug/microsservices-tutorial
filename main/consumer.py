2a3d17a9463ea8fbb5c19af01cfe5db0d067a41f
Vitor Hugo da Silva Lima <vitor.lima@cornershopapp.com>
2021-08-21 14:25:12 -0500

    Adding flask consumer

---
+++ main/consumer.py
@@ -0,0 +1,24 @@
+import pika
+
+
+params = pika.URLParameters(
+    'amqps://zayhgiim:mSZ0s3COp4o1D86Vev_pPxIrVRxGXrcR@cattle.rmq2.cloudamqp.com/zayhgiim'
+)
+
+connection = pika.BlockingConnection(parameters=params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='main')
+
+def callback(channel, method, properties, body):
+    print("Received in main")
+    print(body)
+
+channel.basic_consume(queue='main', on_message_callback=callback)
+
+print("Started consuming")
+
+channel.start_consuming()
+
+channel.close()
