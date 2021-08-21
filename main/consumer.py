656bcd814a1e30f2cfcd7ac33d9df1c11cd921fe
Vitor Hugo da Silva Lima <vitor.lima@cornershopapp.com>
2021-08-21 16:43:14 -0500

    Consuming messages from django application to create, edit and delete products when receive the correct task from django app

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,6 @@
-import pika
+import pika, json
+
+from main import Product, db
 
 
 params = pika.URLParameters(
@@ -13,7 +15,24 @@ channel.queue_declare(queue='main')
 
 def callback(channel, method, properties, body):
     print("Received in main")
-    print(body)
+    data = json.loads(body)
+    print(data)
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print("Product created")
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print("Product updated")
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print("Product deleted")
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
